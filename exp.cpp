185e728958426f2b0ff47e5427f5d0268130f974
root <root@localhost.(none)>
2012-11-20 23:51:32 +0800

    expression to value

--- exp.cpp
+++ exp.cpp
@@ -1,10 +1,48 @@
 #include <stdio.h>
 #include <string>
 #include <stack>
+#include <stdlib.h>
 
 using namespace std;
 
+int compute(int a,int b,char c){
+	switch(c){
+		case '+':
+			return a+b;
+		case '-':
+			return a-b;
+		case '*':
+			return a*b;
+		case '/':
+			return a/b;
+	}
+}
 
+int suffix_to_value(string input){
+	stack<int> s;	
+	string::iterator it;
+	int v = 0;
+	int a = 0;
+	int b = 0;
+	for(it = input.begin(); it < input.end(); ++it){
+		switch(*it){
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+				b = s.top();s.pop();
+				a = s.top();s.pop();
+				v = compute(a,b,*it);
+				s.push(v);
+				break;
+			default:
+				s.push(*it-'0');
+				break;
+		}
+	}
+	return s.top();
+
+}
 string infix_to_suffix(string input){
 	string output;
 	stack<char> s;	
@@ -27,6 +65,13 @@ string infix_to_suffix(string input){
 				break;
 			case '*':
 			case '/':
+				if(!s.empty()){
+					char c = s.top();
+					if(c!='(' && c!='+' && c!='-'){//优先级低于当前操作符不处理
+						output.append(1,c);
+						s.pop();
+					}
+				}
 			case '(':
 				s.push(*it);		
 				break;
@@ -56,6 +101,8 @@ string infix_to_suffix(string input){
 }
 int main(int argc,char *argv[]){
 	string a = infix_to_suffix("3+(2-5)*6/3");
-	printf("%s",a.c_str());
+	printf("%s\n",a.c_str());
+	printf("%d\n",suffix_to_value(a));
+
 	return 0;
 }
